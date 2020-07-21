d0d16411e665881f8016f0d2c1cc2462d18f3f0a
Sakshi7842 <ssingh.sakshi7898@gmail.com>
2020-07-21 13:24:22 +0530

    variable used

---
+++ variables.rb
@@ -0,0 +1,11 @@
+# variables
+puts "Enter you name"
+name=gets.chomp
+puts "Enter your age:"
+age=gets.chomp
+puts "You are #{name}, #{age} years old"
+
+orange=300
+apple=20
+total=orange*apple
+puts "Total value of orange and apple= #{total}"
