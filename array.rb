cf9f2a6eb15e7ea59de1530751cd6774f15b9cd2
Sakshi7842 <ssingh.sakshi7898@gmail.com>
2020-07-21 13:20:32 +0530

    array

---
+++ array.rb
@@ -0,0 +1,10 @@
+fruits = ["apple","orange","banana"] #array
+puts fruits[0]
+puts fruits[1]
+fruits[1] = "watermelon"
+puts fruits[1] 
+puts fruits.size
+fruits << "papaya"
+puts fruits[3]
+puts fruits.size
+name="sakshi  "
